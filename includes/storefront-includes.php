35f855c610f55de037a4cd8e133b4e30b6f2c328
Tyler Redlev <tyler.redlev@gmail.com>
2022-04-03 22:18:02 +0300

    storefront hooks functionalty added

--- includes/storefront-includes.php
+++ includes/storefront-includes.php
@@ -3,14 +3,10 @@
 // Layouts
 
 function remove_woocommerce_action_hooks(){
-    //remove_action( 'woocommerce_archive_description', 'woocommerce_taxonomy_archive_description', 10 );
 
-    //remove_action( 'woocommerce_archive_description', 'woocommerce_product_archive_description', 10 );
+    remove_action("storefront_header", "storefront_header_cart", 60);
     
     remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
-    
-    
-    
     
     remove_action( 'woocommerce_before_shop_loop', 'storefront_sorting_wrapper',  10);
 
@@ -18,14 +14,3 @@ function remove_woocommerce_action_hooks(){
 }
 
 add_action("after_setup_theme", "remove_woocommerce_action_hooks");
-
-function remove_woo_elements(){
-    // Remove the product description
-    //remove_action( 'woocommerce_archive_description', 'woocommerce_taxonomy_archive_description', 10 );
-    //remove_action( 'woocommerce_archive_description', 'woocommerce_product_archive_description', 10 );
-
-  }
-  add_action('wp', 'remove_woo_elements');
-
-
-?>

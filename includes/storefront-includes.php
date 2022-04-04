ae9a27828c16f2e8c80af71e087537e361fe511b
Tyler Redlev <tyler.redlev@gmail.com>
2022-04-04 15:19:37 +0300

    Product search and site branding hooks removed

--- includes/storefront-includes.php
+++ includes/storefront-includes.php
@@ -11,6 +11,10 @@ function remove_woocommerce_action_hooks(){
     remove_action( 'woocommerce_before_shop_loop', 'storefront_sorting_wrapper',  10);
 
     remove_action( 'woocommerce_before_shop_loop', 'storefront_woocommerce_pagination',  30);
+
+    remove_action("storefront_header", "storefront_product_search", 40);
+
+    remove_action("storefront_header", "storefront_site_branding", 20);
 }
 
 add_action("after_setup_theme", "remove_woocommerce_action_hooks");

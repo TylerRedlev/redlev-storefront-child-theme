02133fc3a54bb83d72c03937b0bc1e78349cc7f7
Tyler Redlev <tyler.redlev@gmail.com>
2022-04-16 11:38:30 +0300

    code formatted

--- includes/wc-modifications.php
+++ includes/wc-modifications.php
@@ -5,34 +5,42 @@
 
 
 
-function load_template_layout(){
+function load_template_layout()
+{
 
-        if (is_front_page() || is_tax() || is_product() || is_shop()) {
-            //true
+    if (is_front_page() || is_tax() || is_product() || is_shop()) {
+        //true
 
-                // remove woocommerce sidebar
+        // remove woocommerce sidebar
 
-            remove_action("woocommerce_sidebar", "woocommerce_get_sidebar");
-        }
+        remove_action("woocommerce_sidebar", "woocommerce_get_sidebar");
+
+        //remove_action("woocommerce_before_main_content", "storefront_before_content", 10);
+
+        //remove_action("storefront_sidebar", "storefront_get_sidebar");
+
+    }
 }
- 
+
 
 add_action("template_redirect", "load_template_layout");
 
 
 
 
-function product_after_thumbnail_wrapper_open() {
-    
+function product_after_thumbnail_wrapper_open()
+{
+
     //Open wrapper after thumbnail
     echo '<div class="woo-after-thumbnail-wrapper">';
- }
+}
 
 add_action('woocommerce_before_shop_loop_item_title', 'product_after_thumbnail_wrapper_open', 15);
 
 
 
-function display_desc_in_product_archives() {
+function display_desc_in_product_archives()
+{
 
     //Ecerpt container open
     echo '<div class="woo-excerpt-wrap">';
@@ -42,19 +50,18 @@ function display_desc_in_product_archives() {
 
     //Ecerpt container close
     echo '</div>';
-
-    
- }
- add_action( 'woocommerce_before_shop_loop_item_title', 'display_desc_in_product_archives', 20);
+}
+add_action('woocommerce_before_shop_loop_item_title', 'display_desc_in_product_archives', 20);
 
 
- function product_after_thumbnail_wrapper_close() {
+function product_after_thumbnail_wrapper_close()
+{
 
     //Close wrapper after thumbnail
     echo '</div>';
- }
+}
 
- add_action('woocommerce_after_shop_loop_item', 'product_after_thumbnail_wrapper_close', 7);
+add_action('woocommerce_after_shop_loop_item', 'product_after_thumbnail_wrapper_close', 7);
 
 
 

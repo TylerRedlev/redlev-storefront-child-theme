a30e43ca8fcee99105088e481e9089e3db2cc810
Tyler Redlev <tyler.redlev@gmail.com>
2022-04-16 11:34:53 +0300

    added category template

---
+++ category.php
@@ -0,0 +1,108 @@
+<?php
+
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package storefront
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main site-blog-main" role="main">
+
+
+        <h2 class="blog-page-categories-title">&#127876; Categories &#127876;</h2>
+        <header class="page-header">
+            <?php
+            the_archive_title('<h1 class="page-title">', '</h1>');
+            the_archive_description('<div class="taxonomy-description">', '</div>');
+            ?>
+        </header><!-- .page-header -->
+
+        <?php
+        while (have_posts()) :
+            the_post();
+            do_action('storefront_page_before');
+        ?>
+
+
+<?php  ?>
+
+            <article class="blog-page-article-container">
+
+                <div class="blog-page-thumbnail-container">
+                    <a href="<?php echo get_permalink($post_item['ID']) ?>">
+                        <?php echo get_the_post_thumbnail($post_item['ID'], 'full'); ?>
+
+                    </a>
+                </div>
+
+                <div class="blog-page-short-content-container">
+
+                    <header class="blog-page-entry-header">
+                        <h3 class="blog-page-post-title"><?php echo $post_item['post_title']; ?></h3>
+                        <span class="posted-on">Posted on
+                            <a href="<?php echo get_permalink($post_item['ID']) ?>" rel="bookmark">
+                                <time datetime="<?php echo get_the_date('c', $post_item['ID']); ?>" itemprop="datePublished">
+                                    <?php echo get_the_date('dS M Y', $post_item['ID']); ?></time>
+                            </a>
+                        </span>
+
+                        <span class="blog-page-post-author">by
+                            <a href="http://template-wp.local/author/themedemos/" rel="author">
+                                <?php echo get_avatar(get_the_author_meta($post_item['ID']), 20); ?>
+                                <?php echo get_the_author($post_item['ID']);   ?>
+
+                            </a>
+                        </span>
+
+                    </header>
+
+
+
+                    <div class="blog-page-entry-content">
+
+                        <?php echo mb_strimwidth(get_the_excerpt($post_item['ID']), 0, 220, '...');
+                        //get_the_excerpt($post_item['ID']);  
+                        ?></h4>
+
+                        <?php  //echo mb_strimwidth($post_item['post_content'], 0, 250, '...')
+                        ?>
+
+                        <a href="<?php echo get_permalink($post_item['ID']); ?>">...Read More!</a>
+
+                    </div>
+
+                </div>
+
+            </article>
+
+        <?php
+
+
+
+
+            /**
+             * Functions hooked in to storefront_page_after action
+             *
+             * @hooked storefront_display_comments - 10
+             */
+            do_action('storefront_page_after');
+
+        endwhile; // End of the loop.
+
+    
+        ?>
+
+    </main><!-- #main -->
+</div><!-- #primary -->
+
+<?php
+do_action('storefront_sidebar');
+get_footer();
